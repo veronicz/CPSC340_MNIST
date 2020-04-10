edf1984eda99851aa55bacb4af78d0318c2f656b
sz <v.sherryzzz@gmail.com>
2020-04-09 19:13:16 -0700

    mlp draft

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import argparse
 import gzip
 import os
 import pickle
+import time
 
 import numpy as np
 from numpy.linalg import norm
@@ -9,8 +10,10 @@ from scipy.ndimage import interpolation
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelBinarizer
 
-from knn import KNN
 from deskewing import deskewMNIST
+from knn import KNN
+from mlp_optimizer import optimize
+from neural_net import NeuralNet
 
 
 def load_dataset(filename):
@@ -82,5 +85,53 @@ if __name__ == '__main__':
         y_pred = model.predict(X_test_deskewed)
         test_error = np.mean(y_pred != ytest)
         print("KNN test error for k=%d: %.5f" % (k_opt, test_error))
+
+    elif question == "MLP":
+        X_train_deskewed, y, X_test_deskewed, ytest = loadDeskewedMNIST()
+        binarizer = LabelBinarizer()
+        Y = binarizer.fit_transform(y)
+
+        # hu, batch_size, epochs, lammy, lr = optimize(X_train_deskewed, Y, 5)
+        valid_err_list = [1]
+        hu_list = [500, 700]
+        bs_list = [256, 512, 1024]
+        epochs_list = [100, 300, 500]
+        lambda_list = [0.1, 1e-3]
+        lr_list = [10**-4, 10**-3]
+        min_err = 1
+        hu_opt, bs_opt, epochs_opt, lammy_opt, lr_opt = 0, 0, 0, 0, 0
+        for hu in hu_list:
+            for bs in bs_list:
+                for epochs in epochs_list:
+                    for lammy in lambda_list:
+                        for lr in lr_list:
+                            print("begin:", hu, bs, epochs, lammy, lr)
+                            model = NeuralNet([hu], lammy, lr, bs, epochs)
+                            # val_err = cross_validate_error(
+                            #     model, X_test_deskewed, Y, 1)
+                            model.fit_SGD(
+                                X_train_deskewed[:40000, :], Y[:40000, :])
+                            y_pred = model.predict(X_train_deskewed[40000:, :])
+                            val_err = np.mean(y_pred != y[40000:])
+                            print("end:", val_err)
+                            if (val_err < min_err):  # choose the simper model if error is the same
+                                min_err = val_err
+                                hu_opt, bs_opt, epochs_opt, lammy_opt, lr_opt = hu, bs, epochs, lammy, lr
+                                print(val_err, hu_opt, bs_opt,
+                                      epochs_opt, lammy_opt, lr_opt)
+
+        # hidden_layer_sizes = [600]
+        # model = NeuralNet(hidden_layer_sizes, lammy=lammy,
+        #                   learning_rate=lr, batch_size=256)
+
+        # t = time.time()
+        # model.fit_SGD(X_train_deskewed, Y)
+        # print("Fitting took %d seconds" % (time.time()-t))
+
+        # # Compute test error
+        # yhat = model.predict(X_test_deskewed)
+        # testError = np.mean(yhat != ytest)
+        # print("Test error     = ", testError)
+
     else:
         print("Unknown question: %s" % question)
